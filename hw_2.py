775e35f6c27f11b1601be02f974785f9c35973e9
Mary <57896083+slimmary@users.noreply.github.com>
2019-11-26 16:44:17 +0200

    Update all file
    thought a little and instead of if and eliff used the dictionary operations and labmda function

--- hw_2.py
+++ hw_2.py
@@ -1,5 +1,10 @@
-
-# 1. input a input b input op -> result
+#
+# # 1. input a input b input op -> result
+operations = {'+': lambda a, b: a+b,
+              '-': lambda a, b: a-b,
+              '*': lambda a, b: a*b,
+              '/': lambda a, b: a/b if b != 0 else print('извините, деление на ноль не возможно')
+             }
 i = 5
 while i != 0:
     try:  # пытаемся привести введенные числа к занчению числа, если введено не число, говорим об ошибки и даем всего 5 попыток
@@ -15,25 +20,15 @@ while i != 0:
             print(f'извините вы ввели не математическую операцию, начните заново у вас осталось {i} попыток')
         else:
             i = 0
-            if oper == '/':
-                if b == 0:
-                    print('извините, деление на ноль не возможно')  # в случае деления, проверяем что бы делитель не был = 0
-                else:
-                    res = a / b
-                    print(res)
-            elif oper == '+':
-                res = a + b
-                print(res)
-            elif oper == '-':
-                res = a - b
-                print(res)
-            elif oper == '*':
-                res = a * b
-                print(res)
+            res = operations[oper](a,b)
+            print(res)
 
-
-# 2. #1 +  скобки
-
+# # 2. #1 +  скобки
+operations = {'+': lambda a, b: a+b,
+              '-': lambda a, b: a-b,
+              '*': lambda a, b: a*b,
+              '/': lambda a, b: a/b if b != 0 else print('извините, деление на ноль не возможно')
+             }
 i = 5
 while i != 0:
     try:  # пытаемся привести введенные числа к занчению числа, если введено не число, говорим об ошибки и даем всего 5 попыток
@@ -59,74 +54,6 @@ while i != 0:
                 print(f'это не число, введите, число. у вас осталось {i} попыток')
             else:
                 i = 0
-                if oper == '/':
-                    if b == 0:
-                        print('извините, деление на ноль не возможно') # в случае деления, проверяем что бы делитель в скобках не был = 0
-                    else:
-                        res = a / b  # выполянем операцию в скобках деление
-                        if oper2 == '/':
-                            if e == 0:
-                                print('извините, деление на ноль не возможно') # в случае деления, проверяем что бы делитель за скобками не был = 0
-                            else:
-                                result = res / e  # выполянем операцию после скобок
-                                print(result)
-                        elif oper2 == '+':
-                            result = res + e
-                            print(result)
-                        elif oper2 == '-':
-                            result = res - e
-                            print(result)
-                        elif oper2 == '*':
-                            result = res * e
-                            print(result)
-                elif oper == '+':  # выполянем операцию в скобках сумма
-                    res = a + b
-                    if oper2 == '/':  # в случае деления, проверяем что бы делитель за скобками не был = 0
-                        if e == 0:
-                            print('извините, деление на ноль не возможно')
-                        else:
-                            result = res / e  # выполянем операцию после скобок
-                            print(result)
-                    elif oper2 == '+':
-                        result = res + e
-                        print(result)
-                    elif oper2 == '-':
-                        result = res - e
-                        print(result)
-                    elif oper2 == '*':
-                        result = res * e
-                        print(result)
-                elif oper == '-':  # выполянем операцию в скобках вычитание
-                    res = a - b
-                    if oper2 == '/':
-                        if e == 0:
-                            print('извините, деление на ноль не возможно')  # в случае деления, проверяем что бы делитель за скобками не был = 0
-                        else:
-                            result = res / e  # выполянем операцию после скобок
-                            print(result)
-                    elif oper2 == '+':
-                        result = res + e
-                        print(result)
-                    elif oper2 == '-':
-                        result = res - e
-                        print(result)
-                    elif oper2 == '*':
-                        result = res * e
-                        print(result)
-                elif oper == '*':  # выполянем операцию в скобках умножение
-                    res = a * b
-                    if oper2 == '/':
-                        if e == 0:
-                            print('извините, деление на ноль не возможно')
-                        else:
-                            result = res / e  # выполянем операцию после скобок
-                            print(result)
-                    elif oper2 == '+':
-                        result = res + e
-                        print(result)
-                    elif oper2 == '-':
-                        result = res - e
-                        print(result)
-                    elif oper2 == '*':
-                        result = res * e
-                        print(result)
+                res = operations[oper](a,b)
+                res_all = operations[oper2](res,e)
+                print(res_all)
