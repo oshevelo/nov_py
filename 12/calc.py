b5da9f23af27e69707349b24cb6ab42684b599dd
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2019-11-27 14:21:26 +0200

    Update calc.py

--- 12/calc.py
+++ 12/calc.py
@@ -1,3 +1,4 @@
+# 2.3 var
 from math import *
 
 def repeat(func, message):
@@ -25,3 +26,60 @@ message = 'Enter a statement to calculate:'
 user_input = input(message)
 calculate(user_input)
 repeat(calculate, message)
+
+# 2.2 var
+
+# from math import *
+# 
+# def repeat(func, message):
+#     answer = ''
+#     while answer.upper() != 'N':
+#         answer = input('Do you want to repeat? Enter Y/N:')
+#         if answer.upper() == 'Y':
+#             user_input = input(message)
+#             func(user_input)
+#         elif answer.upper() != 'N':
+#             print('Wrong answer! You should enter Y or N')
+#             continue
+# 
+# def calculate(user_input):
+#     try:
+#         amount_of_nums = int(user_input)
+#         i = 2
+#         result = input('Enter 1 num:')
+#         try:
+#             result = float(result)
+#         except Exception as e:
+#             print('Wrong statement is entered - this should be an integer or float value.')
+#             return None
+#         while i < amount_of_nums + 1:
+#             op = input('Enter an operation (+,-,*,/)').strip()
+#             tmp2 = input(f'Enter {i} num:')
+#             try:
+#                 tmp2 = float(tmp2)
+#                 if op == '+':
+#                     result += tmp2
+#                 if op == '-':
+#                     result -= tmp2
+#                 if op == '*':
+#                     result *= tmp2
+#                 if op == '/':
+#                     try:
+#                         result /= tmp2
+#                     except ZeroDivisionError as e:
+#                         print('Cannot divide by zero.')
+#                         return None
+#             except Exception as exception:
+#                 print('Wrong statement is entered - this should be an integer or float value.')
+#                 return None
+#             i += 1
+#         print(f'Result = {result}')
+#         return result
+#     except Exception as exception:
+#         print('Wrong statement is entered - this should be an integer.')
+#         return None
+# 
+# message = 'Enter an amount of nums to calculate:'
+# user_input = input(message)
+# calculate(user_input)
+# repeat(calculate, message)
