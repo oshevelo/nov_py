ffae4bd7fd8f2866f0fcc2687abb9a884a747d68
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2019-12-06 14:59:12 +0200

    Update calulation.py

--- #3/calulation.py
+++ #3/calulation.py
@@ -2,14 +2,12 @@ from math import e, pi
 import datetime
 
 def convert_to_date(date):
-    patterns = ['%Y%m%d', '%Y-%m-%d', '%Y.%m.%d', '%Y:%m:%d', '%Y/%m/%d', '%Y\%m\%d',
-                '%d%m%Y', '%d-%m-%Y', '%d.%m.%Y', '%d:%m:%Y', '%d/%m/%Y', '%d\%m\%Y']
-    for i in patterns:
-        try:
-            date = datetime.datetime.strptime(date.strip(), i).date()
-            return date
-        except:
-            pass
+    pattern = '%d.%m.%Y'
+    try:
+        date = datetime.datetime.strptime(date.strip(), pattern).date()
+        return date
+    except Exception as e:
+        print(e)
 
 def calculate(user_input):
     try:
@@ -17,14 +15,11 @@ def calculate(user_input):
         if len(date_list) == 2:
             date1 = convert_to_date(date_list[0])
             date2 = convert_to_date(date_list[1])
-        elif len(date_list) == 6: # для 2019-10-10-2018-12-12
-            date1 = convert_to_date(date_list[0] + '.' + date_list[1] + '.' + date_list[2])
-            date2 = convert_to_date(date_list[3] + '.' + date_list[4] + '.' + date_list[5])
-        res = date1 - date2
-        print(f'Result = {res}')
-        return res
+            res = date1 - date2
+            print(f'Result = {res}')
+            return res
     except:
-        pass
+        print('Cannot evaluate statement as a date. Trying to evaluate with arithmetics...')
 
     try:
         res = eval(user_input, {'__builtins__': None}, {'e': e, 'pi': pi})
