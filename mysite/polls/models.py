f0c8405b6e87f6df8ac243878c697c81ef6e06c7
Alex shevelo <oshevelo@softserveinc.com>
2019-12-18 20:36:55 +0200

    fix

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    question_description = models.CharField(max_length=200, null=True)
     pub_date = models.DateTimeField('date published')
 
 
