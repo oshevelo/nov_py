832fa343829ef3b073b35675606de2ca2bbe14a9
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2019-12-25 11:38:37 +0200

    Add files via upload

---
+++ django_project/polls/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=280)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return f'#{self.id} - {self.question_text}'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'#{self.id} - {self.choice_text}'
