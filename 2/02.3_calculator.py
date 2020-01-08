6cf1cc33ca58ed46f38e8ee7a78b26ec03df298b
Jack_oS <s.panchenko@lookout.com>
2020-01-08 12:40:22 +0200

    refactoring

--- 2/02.3_calculator.py
+++ 2/02.3_calculator.py
@@ -1,4 +1,4 @@
-def calculator():
+def calculator():  # TODO ast!
     digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     operators = ['+', '-', '*', '/']
     chars = ['.', 'p', 'i', 'e']
