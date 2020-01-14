2ebd9ff8a573553cace678d63cc6f8c9f3acc1af
Andrii <baranovskyi.and@gmail.com>
2020-01-14 13:15:18 +0200

    add hw7

---
+++ 7/mysite/polls/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question
+from rest_framework import generics
+from .serializers import QuestionSerializer
+
+# Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = QuestionSerializer
+
+    def get_object(self):
+        obj = get_object_or_404(Question, pk=self.kwargs.get('question_id'))
+        return obj
