10e8a88dc3e5d41e101061be13a09393e5113b04
AlisaKoginova <44157617+AlisaKoginova@users.noreply.github.com>
2020-01-25 20:34:00 +0200

    Add files via upload

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
